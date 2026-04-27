d085fdaf7f1f909680e7763c0aaec7107087252d
ArpitAware <arpitaware65@gmail.com>
2026-04-27 10:34:33 +0530

    Fixing CORS config

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",              // local dev
-    "https://neurallearn-theta.vercel.app/"        
+    "https://neurallearn-theta.vercel.app"        
   ],
   credentials: true
 }));
